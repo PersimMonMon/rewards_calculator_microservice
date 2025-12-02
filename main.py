71cb3f1511cc27a0dd54d7fe2245b7eac07696d5
PersimMonMon <buitho@oregonstate.edu>
2025-12-01 20:19:22 -0800

    change port to 5000

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def reward(block_index: int):
 
 if __name__ == "__main__":
     import uvicorn 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
