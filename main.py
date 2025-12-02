18f9060d1235cd9bfb1ef7f13795ad01df686a1d
PersimMonMon <buitho@oregonstate.edu>
2025-12-01 19:23:12 -0800

    have file run if named __main__

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ def reward(block_index: int):
     halving_interval = 2 
     halvings = block_index // halving_interval
     return {"reward": base_reward / (2 ** halvings)}
+
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
